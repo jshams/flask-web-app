3f6828b2850f9d42a114cdffed6aba4b0f8f8534
Jake Shams <jshams62@gmail.com>
2019-02-05 20:34:12 -0800

    added css and html

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 # $ export FLASK_APP=hello.py
 # $ python -m flask run
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+# def hello_world():
+#     return 'Hello, World!'
